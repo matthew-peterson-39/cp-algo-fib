92a9cd22dcbfac27e963dc15709c920df1c2e308
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-03 15:16:12 -0400

    added recursiveFib function to solve problem using recursion.

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,21 @@
+// Using this numbering, the Fibonacci sequence can be defined by the following three equations: 
+// F0 = 0 (applies only to the first integer) 
+// F1 = 1 (applies only to the second integer) 
+// Fn = Fn-1 + Fn-2 (applies to all other integers)
+// Formula sourced from: https://www.techtarget.com/whatis/definition/Fibonacci-sequence 
+
+// For Test Purpose
 function fibonacci(num) {
-  return 0;
+  if(num == 1) { return 0 } 
+  if(num == 2) { return 1 }
+  return (fibonacci(num - 1) + fibonacci(num - 2))
+}
+
+// Recursive Approach
+function fibonacci(num) {
+  if(num == 1) { return 0 } 
+  if(num == 2) { return 1 }
+  return (fibonacci(num - 1) + fibonacci(num - 2))
 }
 
 module.exports = fibonacci;
